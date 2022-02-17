98997dadb9fead6387b6dec32446fa67111b6cb2
will1008m <41597375+whacke@users.noreply.github.com>
2022-02-17 14:34:13 -0600

    Added Library

---
+++ mini-kindle/mini-kindle/Library.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mini_kindle
+{
+    /// <summary>
+    /// Library Class
+    /// </summary>
+    public class Library
+    {
+        /// <summary>
+        /// List of Books in this Library
+        /// </summary>
+        private List<Book> library;
+        /// <summary>
+        /// Library constructor
+        /// </summary>
+        public Library()
+        {
+
+        }
+    }
+}
--- mini-kindle/mini-kindle/mini-kindle.Designer.cs
+++ mini-kindle/mini-kindle/mini-kindle.Designer.cs
@@ -29,10 +29,17 @@ namespace mini_kindle
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Form1";
+            this.Text = "mini-kindle";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
