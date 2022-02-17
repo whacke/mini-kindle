b8c5c16b9f2ff00106ad564bffbee301db0784c1
will1008m <41597375+whacke@users.noreply.github.com>
2022-02-17 14:37:22 -0600

    Added Controller scaffolding

---
+++ mini-kindle/mini-kindle/Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mini_kindle
+{
+    /// <summary>
+    /// Controller class
+    /// </summary>
+    public class Controller
+    {
+        private Library library;
+        private Book curBook;
+        //refresh delegate here
+
+        public Controller()
+        {
+
+        }
+        public void FlipPage()
+        {
+
+        }
+
+        public void HandleBkMk(bool a)
+        {
+
+        }
+    }
+}
